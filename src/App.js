c98fbd59bc07d6b2b1d1554bf3a1bde4c472b633
Esther Park <estherspark91@gmail.com>
2019-08-03 16:42:32 -0400

    useEffect fetchPosts fetching all posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  /*
+  // equivalent to:
+  state = {
+    posts: [],
+  };
+  */
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  // Making the Request - runs when component mounts AND also runs whenever component updates
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const { data } = await axios.get(
+        'http://jsonplaceholder.typicode.com/posts'
+      );
+      setPosts(data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+    // with just this, will be in a never ending loop
+    // vvv to stop that, pass in set of brackets
+    // for this project, will keep [] empty and will mimic componentDidMount lifecycle
+  }, []);
+
   return (
     <div className="container">
       <h1>My App</h1>
